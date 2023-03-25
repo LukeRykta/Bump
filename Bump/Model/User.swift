1a63ce38a5076107d65977d4cce42ac9f70f93d0
genejackett <genejackett@gmail.com>
2023-03-25 18:43:25 -0500

    Add this ass set. yes sir.

---
+++ Bump/Model/User.swift
@@ -0,0 +1,19 @@
+//
+//  User.swift
+//  Bump
+//
+//  Created by wesley jackson on 3/25/23.
+//
+
+import Foundation
+
+struct Person{
+    let profileImage: String
+    let firstName: String
+    let lastName: String
+    let emailAddress: String
+    let phoneNumber: Int
+}
+
+let person1 = Person(profileImage: "person1", firstName: "Wesley", lastName: "Jackson", emailAddress: "wes.jackson@gmail.com", phoneNumber: 3183470543)
+let person2 = Person(profileImage: "person2", firstName: "Ashley", lastName: "Makin", emailAddress: "ash.makin@gmail.com", phoneNumber: 2102227890)
