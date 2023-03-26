f7d29276fef25cf36621fa8ce0e07e9c8f014f0d
Nicholas Schultz <darkmace22@gmail.com>
2023-03-26 00:17:20 -0500

    Added transferString conversion helper function and the new Contact model.

--- Bump Watch App/BluetoothWatchApp/WatchBluetoothModel.swift
+++ Bump Watch App/BluetoothWatchApp/WatchBluetoothModel.swift
@@ -8,6 +8,18 @@
 
 import CoreBluetooth
 import os
+import Foundation
+
+func transferStringtoContact(contactString: String) -> Contact {
+    let contactComponents = contactString.components(separatedBy: ",")
+    
+    let newContact = Contact(phoneNumber: contactComponents[0],
+                firstName: contactComponents[1],
+                lastName: contactComponents[2],
+                email: contactComponents[3])
+                
+    return newContact
+}
 
 class WatchBluetoothModel: NSObject, ObservableObject {
 
---
+++ Bump Watch App/Model/Contact.swift
@@ -0,0 +1,15 @@
+//
+//  Contact.swift
+//  Bump Watch App
+//
+//  Created by Nicholas Schultz on 3/25/23.
+//
+
+import Foundation
+
+struct Contact {
+    var phoneNumber: String
+    var firstName: String
+    var lastName: String
+    var email: String
+}
