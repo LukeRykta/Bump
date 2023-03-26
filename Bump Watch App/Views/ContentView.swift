98a4ff5b82a82f4e833ebb2bd85bdfc61c116167
Nicholas Schultz <darkmace22@gmail.com>
2023-03-25 20:58:01 -0500

    Whitespace change

--- Bump Watch App/Views/ContentView.swift
+++ Bump Watch App/Views/ContentView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     @ObservedObject private var BTModel = WatchBluetoothModel()
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
